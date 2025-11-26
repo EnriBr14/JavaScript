0789201124da930571a5f5b60f0cab5701ddeaef
EnGar <garcianairb14@gmail.com>
2025-11-26 15:37:16 -0600

    Arrays

--- src/JavaScriptCourse/Ejercicios/LigarValores.js
+++ src/JavaScriptCourse/Ejercicios/LigarValores.js
@@ -1,99 +1,35 @@
 let arenasPorLuchador = []
-let empresas = []
-let e1 = {
-    idEm: 1,
-    empresa: "AAA",
-    pais: "México",
-}
-let e2 = {
-    idEm: 2,
-    empresa: "CMLL",
-    pais: "Mexico",
-}
-let e3 = {
-    idEm: 3,
-    empresa: "WWE",
-    pais: "EU",
-}
 
-let e4 = {
-    idEm: 4,
-    empresa: "NXT",
-    pais: "EU",
-}
+let empresas = []
+
+let e1 = {idEm: 1, empresa: "AAA", pais: "México",}
+let e2 = {idEm: 2, empresa: "CMLL", pais: "Mexico",}
+let e3 = {idEm: 3, empresa: "WWE", pais: "EU",}
+let e4 = {idEm: 4, empresa: "NXT", pais: "EU",}
 
 empresas.push(e1,e2,e3,e4)
 //------------------------------------------------------------------------------------
 //1
 let luchadores = []
-let l1 = {
-    id : 1,
-    nombre : "Mistico",
-    idEmpresa: 2
-}
+let l1 = {id : 1, nombre : "Mistico", idEmpresa: 2}
 //2
-let l2 = {
-    id : 2,
-    nombre : "Rey Misterio",
-    numero: "1234",
-    idEmpresa: 3
-}
+let l2 = {id : 2, nombre : "Rey Misterio", numero: "1234", idEmpresa: 3}
 //3
-let l3 = {
-    id : 3,
-    nombre : "Psyco Clown",
-    numero: "1234",
-    idEmpresa: 1
-}
+let l3 = {id : 3, nombre : "Psyco Clown", numero: "1234", idEmpresa: 1}
 //4
-let l4 = {
-    id : 4,
-    nombre : "La Parka",
-    numero: "1234",
-    idEmpresa: 4
-}
+let l4 = {id : 4, nombre : "La Parka", numero: "1234", idEmpresa: 4}
 //5
-let l5 = {
-    id : 5,
-    nombre : "John Cena",
-    numero: "1234",
-    idEmpresa: 3
-}
+let l5 = {id : 5, nombre : "John Cena", numero: "1234", idEmpresa: 3}
 //6
-let l6 = {
-    id : 6,
-    nombre : "Pagano",
-    numero: "1234",
-    idEmpresa: 1
-}
+let l6 = {id : 6, nombre : "Pagano", numero: "1234", idEmpresa: 1}
 //7
-let l7 = {
-    id : 7,
-    nombre : "The Undertaker",
-    numero: "1234",
-    idEmpresa: 3
-}
+let l7 = {id : 7, nombre : "The Undertaker", numero: "1234", idEmpresa: 3}
 //8
-let l8 = {
-    id : 8,
-    nombre : "Mr Iguana",
-    numero: "1234",
-    idEmpresa: 1
-}
+let l8 = {id : 8, nombre : "Mr Iguana", numero: "1234", idEmpresa: 1}
 //9
-let l9 = {
-    id : 9,
-    nombre : "Kemonito",
-    numero: "1234",
-    idEmpresa: 2
-}
+let l9 = {id : 9, nombre : "Kemonito", numero: "1234", idEmpresa: 2}
 //10
-let l10 = {
-    id : 10,
-    nombre : "Hijo de Dr Wagner Jr",
-    numero: "1234",
-    idEmpresa: 4
-}
+let l10 = {id : 10, nombre : "Hijo de Dr Wagner Jr", numero: "1234", idEmpresa: 4}
  luchadores.push(l1,l2,l3,l4,l5,l6,l7,l8,l9,l10)
 
 console.log("Objetos guardados")
--- src/JavaScriptCourse/EstructurasDeDatos/Arrays.js
+++ src/JavaScriptCourse/EstructurasDeDatos/Arrays.js
@@ -14,23 +14,37 @@ luchadores.push("El Santo")
 luchadores.push("Mil Mascaras")
 luchadores.push("Penta 0M")
 
+let poderes = [1,2,3,4,1,4,4,6,2,44,5,6,1,6,65,4,7,8,2,7,5,3]
+
 console.log("Metodos en arrays")
 console.log(luchadores.join("\n"))
 
-//Acceder a un elemento por id
+//Acceder a un elemento por id---------------------------------------------
 console.log("Acceder a un elemento por ID")
 console.log(luchadores[0])
 
-//------------------------------------
+//--------------------------------------------------------------------------
 console.log("Eliminar ultimo elemento del array")
 console.log(luchadores.pop())
-
+//--------------------------------------------------------------------------
 console.log("Eliminar primer elemento del array")
 console.log(luchadores.shift())
 
+//--------------------------------------------------------------------------
 console.log("Nuevo array")
 console.log(luchadores)
+//Filter--------------------------------------------------------------------
+console.log("Metodo filter")
+let menores5 = poderes.filter(num => num <=  5)
+console.log(menores5)
+//----------------Ordenar-----------------------------------------------------
+console.log("Ordenar el array")
+menores5.sort()
+console.log(menores5)
 
-for(let elem of luchadores){
-    console.log(elem)
+//Splice--------------------------------------------------------------------
+console.log("Luchador 2: " + luchadores[0])
+let eliminar = luchadores.splice(0,1)
+for(luch of luchadores){
+    console.log(luch)
 }
